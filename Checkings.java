3022a55f5b1a077c57042f905a9ecb56d4c3bbd9
LilJuiceBox21 <95242821+LilJuiceBox21@users.noreply.github.com>
2024-09-26 17:06:28 -0600

    Add files via upload

--- Checkings.java
+++ Checkings.java
@@ -1,10 +1,41 @@
 class Checkings extends ACCOUNT{
-      int maxMonthlyWithdrawals;
-      public Checkings(double money, int maxMonthlyWithdrawals) {
-          super(money);
-          this.maxMonthlyWithdrawals = maxMonthlyWithdrawals;
-      }
-    public double getMoney(){
-        return super.getMoney();
+    int maxMonthlyWithdrawls;
+    int monthlyWithdrawls;
+    public Checkings(double money, int maxMonthlyWithdrawls) {
+        super(money);
+        this.maxMonthlyWithdrawls = maxMonthlyWithdrawls;
+    }
+
+    public Checkings() {
+    }
+
+    public void setMaxMonthlyWithdrawls(int maxMonthlyWithdrawls) {
+        this.maxMonthlyWithdrawls = maxMonthlyWithdrawls;
+    }
+    public double getMaxMonthlyWithdrawls() {
+        return maxMonthlyWithdrawls;
+    }
+    public void setMonthlyWithdrawals(int monthlyWithdrawls) {
+        this.monthlyWithdrawls = monthlyWithdrawls;
+    }
+    public double getMonthlyWithdrawls() {
+        return monthlyWithdrawls;
+    }
+    public void  withdraw(double withdraw){
+        if(getMonthlyWithdrawls()<=getMaxMonthlyWithdrawls()) {
+            double money = getMoney() - withdraw;
+            setMoney(money);
+            monthlyWithdrawls = +1;
+            if(getMonthlyWithdrawls() == getMaxMonthlyWithdrawls()) {
+                System.out.println("You have reached the maximum monthly withdrawals");
+            }
+        else{
+            System.out.println("You have reached the maximum monthly withdrawals");
+        }
+        }
+    }
+    public void deposit(double deposit){
+        double money = getMoney() + deposit;
+        setMoney(money);
     }
 }
